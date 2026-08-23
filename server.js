8c1b257647ad69cb463c9f9299a237ce4fa35661
lotus64 <lotus64santon@gmail.com>
2026-08-23 10:17:52 +0200

    build: setup orion deploy config and pwa server script

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+
+const wasmDistPath = path.join(__dirname, 'app', 'build', 'dist', 'wasmJs', 'productionExecutable');
+
+app.use(express.static(wasmDistPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(wasmDistPath, 'index.html'));
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Trainflow PWA ${process.env.SERVER_PORT}`);
+});
